9405d31b3cf054e3f74e1ae033decd40a91bd318
marta <mccm20@gmail.com>
2017-10-10 14:34:56 +0100

    Now we only run the map align for non-BAM files (since BAM are already mapped)

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ dic_args = vars(args) # transform the Namespace object into a dictionary where a
 # If reference hasn't been fully indexed, index it
 
 # check if we have BAM files in reads and map non-BAM files (i.e. FASTQ)
+# Need way to deal with non FASTQ files
 not_bam = [(x, True) if x[len(x)-3:len(x)] != "bam" else (0, False) for x in dic_args['reads']]
 if sum([nb[1] for nb in not_bam]) > 0: # if not all items are .bam
     align = Alignment(dic_args['reference'], dic_args['reads'], dic_args['output'])
