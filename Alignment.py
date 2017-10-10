3252ddac9425a86944f8cc4857f1da9822a3d7ac
marta <mccm20@gmail.com>
2017-10-10 14:04:13 +0100

    Added ability to define a custom output folder (Default current directory)

--- Alignment.py
+++ Alignment.py
@@ -1,13 +1,25 @@
 import os
 
 class Alignment:
-    def __init__(self, ref, reads):
+    def __init__(self, ref, reads, output):
         """
         :param ref: the path to the reference genome
         :param reads: list of paths to the reads to map to the genome ref
         """
         self.ref = ref
         self.reads = reads
+        self.out = output
 
     def print_args(self):
         print(self.ref, self.reads)
+
+    def align(self):
+        # executa na mesma quer exista index ou nao - fazer algo acerca disto
+        print("Starting...")
+        os.system('bwa index '+self.ref)
+        print("Done")
+
+    def map(self):
+        for read in self.reads:
+            print(read)
+            os.system('ls '+self.out)
--- main.py
+++ main.py
@@ -8,8 +8,14 @@ Script which runs the pipeline modules.
 parser = argparse.ArgumentParser(description='A basic WES pipeline.')
 parser.add_argument('reference', help="The genome to use as a reference.")
 parser.add_argument('reads', nargs='+', help="The reads to use as input.")
+parser.add_argument('-o', dest='output', default=".", help='Output directory (default is the current directory)')
+# MAYBE adicionar opção para correr no fundo
+# verificar se o ficheiro é BAM ou se é preciso mapear contra o genome de referencia
 args = parser.parse_args() # get args into a Namespace object
 dic_args = vars(args) # transform the Namespace object into a dictionary where argument names are keys
+#print(dic_args)
 
-align = Alignment(dic_args["reference"], dic_args["reads"])
+align = Alignment(dic_args["reference"], dic_args["reads"], dic_args["output"])
 align.print_args()
+#align.align()
+align.map()
