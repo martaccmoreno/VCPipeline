7c7aebf160c3814a98cffc7ab0e4abf51ce8db74
marta <mccm20@gmail.com>
2017-10-10 14:34:34 +0100

    Now we only run the map align for non-BAM files (since BAM are already mapped)

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ parser.add_argument('reference', help="The genome to use as a reference.")
 parser.add_argument('reads', nargs='+', help="The reads to use as input.")
 parser.add_argument('-o', dest='output', default=".", help='Output directory (default is the current directory)')
 # MAYBE adicionar opção para correr no fundo
-# verificar se o ficheiro é BAM ou se é preciso mapear contra o genome de referencia
 args = parser.parse_args() # get args into a Namespace object
 dic_args = vars(args) # transform the Namespace object into a dictionary where argument names are keys
-#print(dic_args)
 
-align = Alignment(dic_args["reference"], dic_args["reads"], dic_args["output"])
-align.print_args()
-#align.align()
-align.map()
+# If reference hasn't been fully indexed, index it
+
+# check if we have BAM files in reads and map non-BAM files (i.e. FASTQ)
+not_bam = [(x, True) if x[len(x)-3:len(x)] != "bam" else (0, False) for x in dic_args['reads']]
+if sum([nb[1] for nb in not_bam]) > 0: # if not all items are .bam
+    align = Alignment(dic_args['reference'], dic_args['reads'], dic_args['output'])
+    #align.map()
+
+
+
