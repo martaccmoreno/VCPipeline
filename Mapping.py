265d9afefa05bd0e77e5cae3fc445bc3ad147559
marta <mccm20@gmail.com>
2017-10-13 16:31:37 +0100

    Added new PostProcessing class as well as a method for Mapping that returns the files created by it in dictionary form.

--- Mapping.py
+++ Mapping.py
@@ -17,6 +17,7 @@ class Mapping:
         self.ref = ref
         self.reads = reads
         self.out = output
+        self.created_files = {}
 
     def print_args(self):  # for testing purposes
         print(self.ref, self.reads)
@@ -31,20 +32,28 @@ class Mapping:
             print('Starting mapping...')
             os.system('bwa mem -R "@RG\tID:foo\tSM:bar\tLB:library1" {} {} > {}/lane.sam'
                       .format(self.ref, self.reads[0], self.out))
+            self.created_files['lane'] = 'lane.sam'
             print('Done')
 
         else:
             print('Starting mapping...')
             os.system('bwa mem -R "@RG\tID:foo\tSM:bar\tLB:library1" {} {} > {}/lane.sam'
                       .format(self.ref, ' '.join(self.reads), self.out))
+            self.created_files['lane'] = 'lane.sam'
             print('Done')
 
     def fixmate(self):
         print('Cleaning up read pairing information and flags...')
         os.system('samtools fixmate -O bam {0}/lane.sam {0}/lane_fixmate.bam'.format(self.out))
+        self.created_files['fixmate'] = 'lane_fixmate.sam'
         print('Done')
 
     def sort(self):
         print('Sorting using coordinate order...')
         os.system('samtools sort -O bam -o {0}/lane_sorted.bam -T {0}/tmp/lane_temp {0}/lane_fixmate.sam'.format(self.out))
+        self.created_files['sorted'] = 'lane_sorted.sam'
         print('Done')
+
+    def return_created_files(self):
+        """Returns the names of the files (as a dictionary) thus far created by the class."""
+        return self.created_files
