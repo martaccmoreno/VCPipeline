7570e172e7e2fcb52bd5c7768638a93a7fa19df5
marta <mccm20@gmail.com>
2017-10-13 15:53:38 +0100

    Renamed class Alignment to Mapping

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Script which runs the pipeline modules.
 """
 
-from Alignment import Alignment
+from Mapping import Mapping
 from misc import *
 import os
 
@@ -23,7 +23,7 @@ dic_args = vars(args)
 index_filenames = gen_index_filenames(dic_args['reference'])
 list_dir = os.listdir(dic_args['output'])
 if check_index_files(list_dir, index_filenames) == False:
-    align = Alignment(dic_args['reference'], dic_args['reads'], dic_args['output'])
+    align = Mapping(dic_args['reference'], dic_args['reads'], dic_args['output'])
     align.index()
 
 
@@ -32,12 +32,12 @@ if check_index_files(list_dir, index_filenames) == False:
 nb = not_bam(dic_args['reads'])
 if sum_not_bam(nb) > 0: # if not all items are .bam
     for nb_elem in nb:
-        to_map = Alignment(dic_args['reference'], nb_elem[0], dic_args['output'])
+        to_map = Mapping(dic_args['reference'], nb_elem[0], dic_args['output'])
         #to_map.map()
         dic_args['reads'] = [read.replace('.sam','.bam') for read in dic_args['reads']]
 
 # next: fixmate and sort
-# We have two instances of Alignment at times... way to only have one instance?
+# We have two instances of Mapping at times... way to only have one instance?
 
 
 
